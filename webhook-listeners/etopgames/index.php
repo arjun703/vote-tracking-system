7d9d6b544cf4a71e0bb234e76871d760aa2565a6
Arjun Poudel <arjunpoudel703@gmail.com>
2024-08-03 22:05:29 +0545

    ....

--- webhook-listeners/etopgames/index.php
+++ webhook-listeners/etopgames/index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<a href="https://www.etopgames.com/"><img src="https://www.etopgames.com/button.php?u=pwember&buttontype=static" alt="Best Game Servers - Top Gaming Servers 100 List | eTopGames" /></a>
+
 <?php
 
 http_response_code(200);
@@ -17,4 +28,9 @@ $userip = isset($_POST['userip']) ? $_POST['userip'] : null;
 if (!is_null($userid)){
     validateAndTakeAppropriateAction($userid, $ip, 'etopgames');
 }
+
+?>
+
+</body>
+</html>
 
