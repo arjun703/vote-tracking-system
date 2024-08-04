daa3cba588f7ec47d65c2aac568ddb7221f1aa6d
Arjun Poudel <arjunpoudel703@gmail.com>
2024-08-04 06:11:14 +0545

    ...

--- test-database/index.php
+++ test-database/index.php
@@ -4,7 +4,7 @@ include './../credentials.php';
 
 $dbc= mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB, $DB_PORT) or die("Error connecting to database");
     
-$query = "SELECT * FROM user_votes  ORDER BY last_voted_at LIMIT 10";
+$query = "SELECT * FROM user_votes  ORDER BY last_voted_at DESC LIMIT 10";
     
 $result = mysqli_query($dbc, $query) or die(mysqli_error($dbc));
 
